15b325f21fce316d7815556514888b1a0a5efefc
Cneale92 <connorneale92@gmail.com>
2024-07-30 18:37:04 -0500

    fixed syntax error with static

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api, api');
-app.use(express.static('public'
+app.use(express.static('public'));
