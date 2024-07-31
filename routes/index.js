d04e0e1d29d315e245d3a32e757b67cdf9415fd2
Cneale92 <connorneale92@gmail.com>
2024-07-30 20:32:48 -0500

    put modular routing code in index js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
-// Test
+// this file was not needed for the challenge but I wanted to practice creating a modular routing file for the future
+
+const router = require('express').Router();
+
+const notesRouter = require('./notes');
+router.use('/notes', notesRouter);
+
+module.exports = router;
