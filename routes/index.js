1832b1cf2db7d213dea44ca10d226582d22aabc8
Cneale92 <connorneale92@gmail.com>
2024-07-30 21:18:28 -0500

    deleted unnecessaary code and started over on server js

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-// this file was not needed for the challenge but I wanted to practice creating a modular routing file for the future
-
-const router = require('express').Router();
-
-const notesRouter = require('./notes');
-router.use('/notes', notesRouter);
-
-module.exports = router;
