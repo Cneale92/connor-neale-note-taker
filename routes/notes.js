3d50feb20a57e754f1418da0ed22a7833c1bd4cb
Cneale92 <connorneale92@gmail.com>
2024-07-30 20:26:34 -0500

    added get for notes js

--- routes/notes.js
+++ routes/notes.js
@@ -1,2 +1,9 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
+
+const { v4: uuidv4 } = require("uuid");
+
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
